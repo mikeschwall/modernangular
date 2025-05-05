35ee7c69c091f537bfc47a245ef152a4c2a78111
Mike Schwall <mikschwall@gmail.com>
2025-05-05 09:17:52 -0500

    clean repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoModule } from './todo/todo.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +11,7 @@ import { TodoModule } from './todo/todo.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TodoModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
