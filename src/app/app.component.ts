59c44261574bffbd3ccf6165e414f696ef255618
Mike Schwall <mikschwall@gmail.com>
2025-05-19 08:55:56 -0500

    clean repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { CourseService } from './todo/course.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,26 +6,7 @@ import { CourseService } from './todo/course.service';
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit,AfterViewInit {
-  title = 'apr28';
-
-  mydata:any;
-
-  constructor(private courseService:CourseService) {}
-
-  ngOnInit(): void {
-    
-  }
-
-  getdata(test:any) {
-    this.courseService.getEmployee(test).subscribe(item => this.mydata = item);
-  }
-
-ngAfterViewInit(): void {
-}
-
-final(test:any) {
-  console.log(test);
-}
+export class AppComponent  {
+  
 
 }
