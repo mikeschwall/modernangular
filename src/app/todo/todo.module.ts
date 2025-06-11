59a70a2c92986e448d154aea36af5d270ae6c748
Mike Schwall <mikschwall@gmail.com>
2025-06-11 10:17:57 -0500

    feature commit

---
+++ src/app/todo/todo.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TodoRoutingModule } from './todo-routing.module';
+import { AlertComponent } from './alert/alert.component';
+import { OneComponent } from './one/one.component';
+import { InitComponent } from './init/init.component';
+import { TwoComponent } from './two/two.component';
+import { ThreeComponent } from './three/three.component';
+
+
+@NgModule({
+  declarations: [
+    AlertComponent,
+    OneComponent,
+    InitComponent,
+    TwoComponent,
+    ThreeComponent
+  ],
+  imports: [
+    CommonModule,
+    TodoRoutingModule
+  ]
+})
+export class TodoModule { }
