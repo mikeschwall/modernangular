f4dbc67d66b9077a1ddca317cba21a343506ac60
Mike Schwall <mikschwall@gmail.com>
2025-11-03 08:22:59 -0600

    clean repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { CourseService } from './todo/course.service';
 
 
 @Component({
@@ -12,7 +11,7 @@ export class AppComponent implements OnInit,AfterViewInit  {
 
 mydata:any;
   
-  constructor(private courseService:CourseService) {
+  constructor() {
 
   }
 
@@ -25,7 +24,7 @@ mydata:any;
   }
 
   getdata(test:any) {
-    this.courseService.getPerson(test).subscribe(item => this.mydata = item)
+
   }
 
   final(test:any) {
