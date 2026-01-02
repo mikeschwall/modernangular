e008c5d4824fdd5596f6b23422295e073819f258
Mike Schwall <mikschwall@gmail.com>
2026-01-02 08:19:54 -0600

    add feature

---
+++ src/app/todo/init/init.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-init',
+  standalone: false,
+  templateUrl: './init.component.html',
+  styleUrl: './init.component.css'
+})
+export class InitComponent {
+
+}
---
+++ src/app/todo/two/two.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two',
+  standalone: false,
+  templateUrl: './two.component.html',
+  styleUrl: './two.component.css'
+})
+export class TwoComponent {
+
+}
