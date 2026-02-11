5c56fb1da400a0e664852118b62a3a0965550fab
Mike Schwall <mikschwall@gmail.com>
2026-02-11 08:50:32 -0600

    feature commit

---
+++ src/app/todo/two/two.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { CourseService } from '../course.service';
+
+@Component({
+  selector: 'two',
+  standalone: false,
+  templateUrl: './two.component.html',
+  styleUrl: './two.component.css'
+})
+export class TwoComponent implements OnInit {
+
+  mygroup!:FormGroup;
+
+  constructor(private courseService:CourseService) {
+
+  }
+
+  ngOnInit(): void {
+    this.mygroup = new FormGroup({
+      food: new FormControl(null)
+    })
+    
+  }
+
+  onClick() {
+    this.courseService.sendData(this.mygroup.value.food).subscribe(() => console.log("sent"))
+  }
+
+}
